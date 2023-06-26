901986f3f8fb3f13b025b1175b491ca6c8800457
augusto <augusdpmenezes@gmail.com>
2023-06-26 10:49:35 -0300

    alteração das urls

--- Helpers/EmailBody.cs
+++ Helpers/EmailBody.cs
@@ -14,7 +14,7 @@ namespace UserAuth.Helpers
 <div stlye=""display: flex; flex-direction: column; justify-content: center; align-content: center; align-items: center; gap: 20px;"">
 <h1>Redefina a sua senha</h1>
 <p>Clique no botão abaixo para ir à página de redefinição de senha.</p>
-<a  href=""http://localhost:4200/reset?email={email}&code={emailToken}"" target=""_blank"" style=""padding: 10px 20px; background: #e7b630; border-radius: 3px; width: 50%; text-align: center; text-decoration: none;"">Redefinir senha</a>
+<a  href=""https://projeto-foods-aughustuss.vercel.app/reset?email={email}&code={emailToken}"" target=""_blank"" style=""padding: 10px 20px; background: #e7b630; border-radius: 3px; width: 50%; text-align: center; text-decoration: none; color: black;"">Redefinir senha</a>
 </div>
 </body>
 </html>";
